c3faa199f8c825fdbd06951ad70c0c0b9d19abe7
ar2r-java <roooob66666666666@gmail.com>
2022-09-15 16:29:23 +0300

    createFormAdd

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Redactor</title>
+</head>
+<body>
+    <form class="form_add" action="add.php" method="post">
+        Add info about new employee<br>
+        Input First name <input type="text"><br>
+        Input Last name <input type="text"><br>
+        Input Date of Birth <input type="date"><br>
+        Salary <input type="text"><br>
+        <button>Submit</button>
+    </form>
+</body>
+</html>
