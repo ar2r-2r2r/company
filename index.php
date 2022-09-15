220ddb37197bdc16ad091badc6f2b43976781e68
ar2r-java <roooob66666666666@gmail.com>
2022-09-15 22:15:32 +0300

    addFormDel

--- index.php
+++ index.php
@@ -37,14 +37,21 @@
     </table>
    
     <form class="form_add" action="add.php" method="post">
-        Add info about new employee<br>
-        Input First name <input type="text" name="add_fname"><br>
-        Input Last name <input type="text" name="add_lname" ><br>
-        Input Date of Birth <input type="date" name="add_dob" ><br>
-        Salary <input type="text" name="add_salary" ><br>
-        <button>Submit</button>
+        <span class="span_add" id="info" >Add info about employer to database</span><br>
+        <span class="span_add">Enter first name</span> <input type="text" name="add_fname"><br>
+        <span class="span_add">Enter last name</span> <input type="text" name="add_lname" ><br>
+        <span class="span_add">Enter date of birth</span> <input type="date" name="add_dob" ><br>
+        <span class="span_add">Enter salary</span> <input type="text" name="add_salary" ><br>
+        <div class="btn"><button type="submit">Submit</button></div>
     </form>
 
-
+    <form class="form_del" action="add.php" method="post">
+        <span class="span_dell" id="info" >Delete info about employer</span><br>
+        <span class="span_dell">Enter first name</span> <input type="text" name="del_fname"><br>
+        <span class="span_dell">Enter last name</span> <input type="text" name="del_lname" ><br>
+        <span class="span_dell">Enter date of birth</span> <input type="date" name="del_dob" ><br>
+        <span class="span_dell">Enter salary</span> <input type="text" name="del_salary" ><br>
+        <div class="btn"><button type="submit">Submit</button></div>
+    </form>
 </body>
 </html>
