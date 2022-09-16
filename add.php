50016b01cdbc81e1f94d6555cac1a4cde6d0ed29
ar2r-java <roooob66666666666@gmail.com>
2022-09-16 20:47:47 +0300

    psr

--- add.php
+++ add.php
@@ -3,11 +3,11 @@ spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });
 
-$fname=$_POST['add_fname'];
-$lname=$_POST['add_lname'];
-$dob=$_POST['add_dob'];
-$salary=settype($_POST['add_salary'], 'integer');
+$fname = $_POST['add_fname'];
+$lname = $_POST['add_lname'];
+$dob = $_POST['add_dob'];
+$salary = $_POST['add_salary'];
 // $employer=new Employer($fname,$lname,$dob,$salary);
-$db=new Database;
+$db = new Database;
 $db->execute("INSERT INTO `employer` SET `firstName`='$fname', `lastName`='$lname', `dob`='$dob', `salary`='$salary'");
-header('Location: /');         //переход на главную
+header('Location: /'); //переход на главную
